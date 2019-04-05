6dd85e6910ee7d9d97b8d742e8136a99d1f87311
stanleyuan <stanley830912@gmail.com>
2019-04-05 16:39:27 +0800

    w1(linked_list): bubble sort

--- w1/linked_list/bubble_sort.c
+++ w1/linked_list/bubble_sort.c
@@ -61,6 +61,18 @@ void swap(struct node **start, struct node *node1, struct node *node2) {
 		*start = node2;
 }
 
+void bubble_sort(struct node **start) {
+	struct node *counter1 = *start;
+
+	for (; counter1 != (*start)->prev; counter1 = counter1->next) {
+		for (struct node *counter2 = *start; \
+				counter2 != (*start)->prev; counter2 = counter2->next) {
+			if (counter2->data > counter2->next->data)
+				swap(start, counter2, counter2->next);
+		}
+	}
+}
+
 void display(struct node *start) {
     struct node *temp = start;
     printf("Traversal in forward direction \n");
@@ -77,9 +89,18 @@ void display(struct node *start) {
 
 int main() {
     struct node *start = NULL;
-    insert_tail(&start, 51); insert(&start, 48);
-    insert_tail(&start, 72); insert_tail(&start, 86);
-    insert_after(&start, 63, 51);
+    insert_tail(&start, 5);
+	insert_tail(&start, 9);
+	insert_tail(&start, 8);
+	insert_tail(&start, 2);
+	insert_tail(&start, 2);
+	insert_tail(&start, 7);
+	insert_tail(&start, 6);
+	insert_tail(&start, 4);
+	insert_tail(&start, 2);
+    display(start);
+
+	bubble_sort(&start);
     display(start);
     return 0;
 }
